7c6bbc261b0f04e390c066ff77848ac2ec70b57f
Hirokai Bandai <totemham.4230@gmail.com>
2020-10-10 19:09:07 +0900

    finish auth part and concat to our frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NotFound from "./components/NotFound";
@@ -7,29 +18,42 @@ import "./css/main.scss";
 import MovieList from "./components/movieList";
 import AddMovie from "./components/addMovie";
 import ListMovie from "./components/listMovie";
-
-
-import axios from "axios";
-
-
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+// Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 class App extends Component {
-
   render() {
     return (
-      <>
-        <BrowserRouter>
-          <Header />
-          <div className="content-main-container">
-            <Switch>
-              <Route exact path="/" component={ListMovie} />
-              <Route exact path="/addMovie" component={AddMovie} />
-            </Switch>
-          </div>
-          <Footer />
-        </BrowserRouter>
-      </>
+        <Provider store={store}>
+          <Router>
+            <div className="App">
+              <Navbar />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              {/*<Route path="*" component={NotFound}/>*/}
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+              </Switch>
+            </div>
+          </Router>
+        </Provider>
     );
   }
 }
-
 export default App;
