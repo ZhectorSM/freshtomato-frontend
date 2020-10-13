e445058687f8d45e88c7508b35b741a97ed22c15
Shu <stbook.syu@icloud.com>
2020-10-12 18:10:05 -0700

    Flame fix

--- src/components/listMovie.jsx
+++ src/components/listMovie.jsx
@@ -1,53 +1,49 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../css/listMovie.scss";
-import StarRating from "../components/rating/StarRating"
-
+import StarRating from "../components/rating/StarRating";
 
 const ListMovie = (req, res) => {
   const [listMovies, setListMovies] = useState([]);
 
-  useEffect(() => {    
-
-
-
+  useEffect(() => {
     var config = {
-      method: 'get',
-      url: 'http://localhost:8000/admin/listMovies',
+      method: "get",
+      url: "http://localhost:8000/admin/listMovies",
       headers: {
-          'x-auth-token': localStorage.getItem('jwtToken')
+        "x-auth-token": localStorage.getItem("jwtToken")
       }
     };
- 
-   axios(config)
-    .then(result => {
-      console.log(result.data);
-      setListMovies(result.data);
-    })
-    .catch(err => {
-      console.log(err);
-    });        
-  },[]);
 
-
- 
+    axios(config)
+      .then(result => {
+        console.log(result.data);
+        setListMovies(result.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
       {listMovies.map(list => (
-        <article className="movieContainer">
+        <div className="movieContainer">
           <div className="img-container">
-            <img src={list.urlImage} alt="" height="250px" />
-            <h3>{list._id}</h3>
-            <h3>{list.name}</h3>
-            <h4>{list.description}</h4>
-            <p>{list.length}</p>
-            <h5>category:{list.category}</h5>
-            <h5>{list.rate}</h5>
-            <StarRating movieId={list._id}/>
+            {/* <h3>{list._id}</h3> */}
+            <h4>{list.name}</h4>
+            <img src={list.urlImage} alt="" />
+            <p>{list.description}</p>
+            <h6>
+              category:{list.category}{" "}
+              <span className="zoo">rate: {list.rate} </span>
+              <span className="foo">{list.length}min</span>
+            </h6>
+            <StarRating className="star" movieId={list._id} />
+            <button onclick="activateLasers()">Edit</button>
+            <button onclick="activateLasers()">Delete</button>
           </div>
-        </article>
+        </div>
       ))}
     </>
   );
